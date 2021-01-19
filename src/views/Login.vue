9657e0b6b585c99766a6d54175bbed64c463d5fe
Jeff-wu-hub(mac) <15809812381@163.com>
2021-01-19 22:45:15 +0800

    上传vue目录结构

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="app">
+  <button @click="test">按钮</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+    async test(){
+      const { data } = this.$http.get('/hello')
+      alert(data)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
